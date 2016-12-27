e5da102b9d1814190ae97bc817f47079bbb83398
Laszlo Balogh <laszlo@thebaloghs.com>
2016-12-27 10:34:02 -0700

    Add width config interface float handling

--- app/models/definitions.rb
+++ app/models/definitions.rb
@@ -3,9 +3,11 @@ module Definitions
   PARAM_CONTROL = {"main_switch_status" => :main_switch_status,
                   "main_switch" => :main_switch,
                   "last_delay_tab" => :last_delay_tab,
-                  "channel_a_delay" => :channel_a_delay,
                   "channel_a_name_custom" => :channel_a_name_custom,
+                  "channel_a_delay" => :channel_a_delay,
                   "channel_a_delay_step_size" => :channel_a_delay_step_size,
+                  "channel_a_width" => :channel_a_width,
+                  "channel_a_width_step_size" => :channel_a_width_step_size,
                   "channel_b_delay" => :channel_b_delay,
                   "channel_b_name_custom" => :channel_b_name_custom,
                   "channel_b_delay_step_size" => :channel_b_delay_step_size,
@@ -46,13 +48,13 @@ module Definitions
   DEFAULT_PN_DELAY_UNIT = "ns"
   DEFAULT_PN_DELAY_MIN = "0" #ns
   DEFAULT_PN_DELAY_MAX = "100000" #ns
-  DEFAULT_PN_WIDTH = "2"  #us
+  DEFAULT_PN_WIDTH = "2.00"  #us
   DEFAULT_PN_WIDTH_UNIT = "us"
-  DEFAULT_PN_WIDTH_MIN = "1"  #us
-  DEFAULT_PN_WIDTH_MAX = "100"  #us
+  DEFAULT_PN_WIDTH_MIN = "1.00"  #us
+  DEFAULT_PN_WIDTH_MAX = "100.00"  #us
   DEFAULT_PN_POLARITY = "NORM"
   DEFAULT_DELAY_STEP_SIZE = "10"  #ns
-  DEFAULT_WIDTH_STEP_SIZE = "2"  #us
+  DEFAULT_WIDTH_STEP_SIZE = "0.1"  #us
 
   #Serial Command Definitions:
   MAIN_OUTPUT_ON = ":INST:STATE ON"
@@ -61,6 +63,7 @@ module Definitions
   P2_DELAY = ":PULSE2:DELAY" #time OR ?
   P3_DELAY = ":PULSE3:DELAY" #time OR ?
   P4_DELAY = ":PULSE4:DELAY" #time OR ?
+  P1_WIDTH = ":PULSE1:WIDTH" #time OR ?
   P1_MODE = ":PULSE1:POL" #config OR ?
   P2_MODE = ":PULSE2:POL" #config OR ?
   P3_MODE = ":PULSE3:POL" #config OR ?
